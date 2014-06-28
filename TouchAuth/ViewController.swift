aa37ed12bdd98f2742fcb40c4057577129a58f7b
Zoya Pribytkova <niakris90@gmail.com>
2014-06-28 16:51:35 +0400

    Initial app commit

---
+++ TouchAuth/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TouchAuth
+//
+//  Created by Прибыткова Зоя on 28.06.14.
+//  Copyright (c) 2014 Прибыткова Зоя. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
