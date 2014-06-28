270512ba9b5c43ea29c639efdd879420feebcaf2
VladiMihaylenko <vxmihaylenko@gmail.com>
2014-06-28 19:14:51 +0400

    Add action for button

--- TouchAuth/ViewController.swift
+++ TouchAuth/ViewController.swift
@@ -7,19 +7,43 @@
 //
 
 import UIKit
+import Accounts
 
 class ViewController: UIViewController {
     @IBOutlet var tapMeButton:UIButton
                             
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let accounts:NSURLCredentialStorage! = NSURLCredentialStorage.sharedCredentialStorage();
+        if accounts.allCredentials.objectForKey("OurTestService") == nil {
+            self.tapMeButton.setTitle("Join", forState:UIControlState.Normal)
+        } else {
+            self.tapMeButton.setTitle("Sign In", forState:UIControlState.Normal)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func tapMeButtonTap(sender:UIButton) {
+        if sender.titleLabel.text == "Sign In" {
+            processingAuthentification()
+        } else {
+           processingRegistration()
+        }
+    }
+    
+    func processingAuthentification() {
+        
+    }
+    
+    func processingRegistration() {
+        
+    }
+    
+    
 
 
 }
