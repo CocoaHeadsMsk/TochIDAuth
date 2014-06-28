b6d27091c199eef75e7afa340a7ac44b38853afc
VladiMihaylenko <vxmihaylenko@gmail.com>
2014-06-28 17:19:59 +0400

    Added TapMe button

--- TouchAuth/ViewController.swift
+++ TouchAuth/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var tapMeButton:UIButton
                             
     override func viewDidLoad() {
         super.viewDidLoad()
